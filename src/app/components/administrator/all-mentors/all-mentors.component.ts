45c81b50b2baf35720ab45313487b6112c02da11
ivxxi <crystalalice21@gmail.com>
2020-08-02 11:57:58 +0300

    adds all-mentors component

---
+++ src/app/components/administrator/all-mentors/all-mentors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-mentors',
+  templateUrl: './all-mentors.component.html',
+  styleUrls: ['./all-mentors.component.css']
+})
+export class AllMentorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
