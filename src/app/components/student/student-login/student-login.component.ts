e9dc8bd135ea217e30dff49b3b76cfe619231ae4
ivxxi <crystalalice21@gmail.com>
2020-08-04 23:48:15 +0300

    adds student login component with functionalities

--- src/app/components/student/student-login/student-login.component.ts
+++ src/app/components/student/student-login/student-login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/api.service'; 
+import { Router } from '@angular/router';
+import {  StudentService } from '../../../services/student/student.service';
 
 @Component({
   selector: 'app-student-login',
   templateUrl: './student-login.component.html',
-  styleUrls: ['./student-login.component.css']
+  styleUrls: ['./student-login.component.css'],
+  providers: [ApiService, StudentService]
 })
 export class StudentLoginComponent implements OnInit {
 
-  constructor() { }
+  studentdata;
+  error: any;
 
-  ngOnInit(): void {
+  constructor(private api:ApiService, private router:Router, private studentService: StudentService) { }
+
+  ngOnInit(){
+    this.studentdata = {
+      email: '', password: ''
+    };
   }
 
+  studentLogin(email: string, password: string) {
+    this.studentService.studentLogin(email, password).subscribe(
+      success => this.router.navigate(['/student/dashboard']),
+      error => this.error = error
+    );
+  }
 }
