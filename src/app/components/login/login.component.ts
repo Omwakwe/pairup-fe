5e1eff230815920d154487b0b2c1a715996d1487
Lorna Kamau <kamaulorna@gmail.com>
2020-08-04 19:16:45 +0300

    restructure login route

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../../services/admin/admin.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AdminService],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  admindata;
+  error: any;
+  
+  constructor(private router:Router, private adminService: AdminService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.admindata = {
+      email: '', password: ''
+    };
+  }
+
+  adminLogin(email: string, password: string) {
+    this.adminService.adminLogin(email, password).subscribe(
+      success => this.router.navigate(['/admin/dashboard']),
+      error => this.error = error
+    );
   }
 
 }
