e6180dd0ac17be3b70991e5998f7865fed682d47
ivxxi <crystalalice21@gmail.com>
2020-08-05 23:41:28 +0300

    adds administrator component tests

--- src/app/components/administrator/administrator.component.spec.ts
+++ src/app/components/administrator/administrator.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AdministratorComponent } from './administrator.component';
 
 describe('AdministratorComponent', () => {
@@ -8,7 +9,8 @@ describe('AdministratorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdministratorComponent ]
+      declarations: [ AdministratorComponent ],
+      imports:[ RouterTestingModule, HttpClientTestingModule ]
     })
     .compileComponents();
   }));
