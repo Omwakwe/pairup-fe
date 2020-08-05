0c436b87fb3cbf94c159fec5624d8d0d593b9f9b
ivxxi <crystalalice21@gmail.com>
2020-08-05 23:37:12 +0300

    adds admin login tests

--- src/app/components/administrator/admin-login/admin-login.component.spec.ts
+++ src/app/components/administrator/admin-login/admin-login.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AdminLoginComponent } from './admin-login.component';
 
 describe('AdminLoginComponent', () => {
@@ -8,7 +9,8 @@ describe('AdminLoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminLoginComponent ]
+      declarations: [ AdminLoginComponent ],
+      imports:[ RouterTestingModule, HttpClientTestingModule ]
     })
     .compileComponents();
   }));
